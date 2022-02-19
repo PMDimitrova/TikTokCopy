3e0178e6adfeda613f3720c7c912911c3c1c472c
PMDimitrova <palmina.dimitrova@gmail.com>
2022-02-20 01:02:19 +0200

    login functionality implemented

--- tiktok-copy/src/components/LoginRegister/LoginWithEmail.js
+++ tiktok-copy/src/components/LoginRegister/LoginWithEmail.js
@@ -1,8 +1,13 @@
 import styles from "./Login.module.scss";
 import {GrClose} from "react-icons/gr";
 import {useDispatch} from "react-redux";
+import {useState} from "react";
+import {Alert} from "@mui/material";
 
 export default function LoginWithEmail(props){
+    const [logInBtnState, enableBtn] = useState(true);
+    const [wrongCredentials, showWarningCredentials] = useState(false);
+
     const dispatch = useDispatch();
 
     const handleShowLoginWithEmail = () => {
@@ -12,6 +17,42 @@ export default function LoginWithEmail(props){
 
     const showRegisterDialog = () => {
         dispatch({type : 'SHOW_REGISTER'});
+    }
+
+    const checkIfBothAreFilled = () => {
+        if(document.getElementById('usernameInputField').value.length && document.getElementById('passwordInputField').value.length){
+            if (logInBtnState){
+                enableBtn(!logInBtnState);
+            }
+            document.getElementById('logInButton').className = styles.logInBtnEnabled;
+        }
+    }
+
+    const logUser = () => {
+        console.log('clicked log user btn');
+        let username = document.getElementById('usernameInputField').value.trim();
+        let password = document.getElementById('passwordInputField').value.trim();
+
+        fetch('https://tiktok-635d3-default-rtdb.firebaseio.com/users.json')
+            .then(res => res.json())
+            .then(users => {
+                let incorrectPassword = false;
+                let userExists = false;
+
+                for (const [us, details] of Object.entries(users)) {
+                    if (us === username){
+                        if(details.password === password){
+                            dispatch({type : 'LOGIN', payload: username});
+                        }else {
+                            incorrectPassword = true;
+                        }
+                        userExists = true;
+                    }
+                }
+                if (incorrectPassword || !userExists){
+                    showWarningCredentials(!wrongCredentials);
+                }
+            });
     }
 
     return (
@@ -26,12 +67,15 @@ export default function LoginWithEmail(props){
                             <div>Log in with phone</div>
                         </div>
                         <form>
-                            <input className={styles.loginInputField} placeholder={'Email or Username'}/>
-                            <input className={styles.loginInputField} placeholder={'Password'} />
+                            <input id={'usernameInputField'} className={styles.loginInputField}
+                                   placeholder={'Email or Username'} onChange={checkIfBothAreFilled}/>
+                            <input id={'passwordInputField'} className={styles.loginInputField}
+                                   placeholder={'Password'} type={'password'} onChange={checkIfBothAreFilled}/>
                         </form>
                     <div className={styles.loginForgotPass}>Forgot password?</div>
-                    <button className={styles.loginBtn}>Log in</button>
+                    <button id={'logInButton'} className={styles.loginBtnDisabled} disabled={logInBtnState} onClick={logUser}>Log in</button>
                     </div>
+                    {wrongCredentials ? <Alert variant="outlined" severity="error" style={{marginTop:12}}>Wrong credentials</Alert> : <></>}
                 </div>
             </div>
             <div className={styles.loginFooterContainer}>

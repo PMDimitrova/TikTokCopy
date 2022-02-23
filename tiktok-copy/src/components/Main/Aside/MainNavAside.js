5081d1e1879d98b66665c21efb9034b5bb7c980b
PMDimitrova <palmina.dimitrova@gmail.com>
2022-02-24 00:05:03 +0200

    aside icons changed & css changes

--- tiktok-copy/src/components/Main/Aside/MainNavAside.js
+++ tiktok-copy/src/components/Main/Aside/MainNavAside.js
@@ -1,30 +1,30 @@
-import homeLogo from '../../../images/tiktok-aside-mainNav-iconHome.PNG';
-import followingLogo from '../../../images/tiktok-aside-mainNav-iconFollowing.PNG';
-import liveLogo from '../../../images/tiktok-aside- mainNav-iconLive.PNG';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {RiHome4Line} from "react-icons/ri";
+import {BsPeople} from "react-icons/bs";
+import {VscDeviceCameraVideo} from "react-icons/vsc";
 
-export default function MainNavAside(){
-    return(
+export default function MainNavAside() {
+    return (
         <div className='MainNavAside'>
             <div className='MainAsideBtn'>
                 <Link to='/' className='MainBtn'>
-                        <img className='MainBtnLogo' src={homeLogo} alt='MainBtnLogo'/>
-                        <h2 className='MainBtnText'>For You</h2>
+                    <RiHome4Line className={'MainAsideTopIcons'}/>
+                    <h2 className='MainBtnText'>For You</h2>
                 </Link>
             </div>
             <div className='MainAsideBtn'>
                 <Link to='/' className='MainBtn'
-//***PALMINA to do onClick={go to under construction}
+                    //***PALMINA to do onClick={go to under construction}
                 >
-                    <img className='MainBtnLogo' src={followingLogo} alt='MainBtnLogo'/>
+                    <BsPeople className={'MainAsideTopIcons'}/>
                     <h2 className='MainBtnText'>Following</h2>
                 </Link>
             </div>
             <div className='MainAsideBtn'>
                 <Link to='/LIVE' className='MainBtn'
-//***PALMINA to do onClick={go to under construction}
->
-                    <img className='MainBtnLogo' src={liveLogo} alt='MainBtnLogo'/>
+                    //***PALMINA to do onClick={go to under construction}
+                >
+                    <VscDeviceCameraVideo className={'MainAsideTopIcons'}/>
                     <h2 className='MainBtnText'>LIVE</h2>
                 </Link>
             </div>

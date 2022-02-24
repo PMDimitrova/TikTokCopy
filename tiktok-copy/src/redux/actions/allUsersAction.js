afdc3b4b7670badb202b8662397959e4ee164f76
PMDimitrova <palmina.dimitrova@gmail.com>
2022-02-24 03:18:25 +0200

    allUsersReducer minor changes

--- tiktok-copy/src/redux/actions/allUsersAction.js
+++ tiktok-copy/src/redux/actions/allUsersAction.js
@@ -1,20 +1,24 @@
 export const getAllUsers = () => {
-    let allUsersArr = [];
-    return function(dispatch){
-        fetch(`https://tiktok-635d3-default-rtdb.firebaseio.com/users.json`)
-        .then(resp => resp.json())
-        .then(data => {allUsersArr = Object.values(data)
-            console.log(allUsersArr)
-        dispatch({type : 'GET_ALL_USERS', payload : allUsersArr}) } )
-    }
+
+
+    // let allUsersArr = [];
+    // return function (dispatch) {
+    //     fetch(`https://tiktok-635d3-default-rtdb.firebaseio.com/users.json`)
+    //         .then(resp => resp.json())
+    //         .then(data => {
+    //             allUsersArr = Object.values(data)
+    //             console.log('ALL USERS:' + allUsersArr)
+    //             dispatch({type: 'GET_ALL_USERS', payload: allUsersArr})
+    //         })
+    // }
 }
 
 export const toggleFollow = (user) => {
-    return function(dispatch){
-        fetch(`https://tiktok-635d3-default-rtdb.firebaseio.com/users/${user.username}/iFollow.json`, 
-        {method: 'PUT', body: JSON.stringify(user.iFollow)})
+    return function (dispatch) {
+        fetch(`https://tiktok-635d3-default-rtdb.firebaseio.com/users/${user.username}/iFollow.json`,
+            {method: 'PUT', body: JSON.stringify(user.iFollow)})
             .then(resp => resp.json())
             // .then(data => console.log(data))
-            .then(data => dispatch({type : 'TOGGLE_FOLLOW', payload : {iFollow: data}}));
+            .then(data => dispatch({type: 'TOGGLE_FOLLOW', payload: {iFollow: data}}));
     }
 }
--- tiktok-copy/src/redux/reducers/allUsersReducer.js
+++ tiktok-copy/src/redux/reducers/allUsersReducer.js
@@ -1,5 +1,6 @@
 const INITIAL_STATE = {
-    users: []
+    users: [],
+    usernamesAndProfilePictures: []
 };
 
 export const allUsersReducer = (state = INITIAL_STATE, action) => {

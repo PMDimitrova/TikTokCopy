e3bcb70f7f0ce0258be1c6b5fb5a90375dc2a28c
PMDimitrova <palmina.dimitrova@gmail.com>
2022-02-25 02:19:42 +0200

    UPLOAD fully implemented

--- tiktok-copy/src/components/Main/Upload/UploadVideoComponent.js
+++ tiktok-copy/src/components/Main/Upload/UploadVideoComponent.js
@@ -1,10 +1,105 @@
 import styles from './UploadVideo.module.scss';
-import {Checkbox, FormControl, FormControlLabel, MenuItem, Select, Switch} from "@mui/material";
+import {Alert, Checkbox, FormControl, FormControlLabel, MenuItem, Select, Switch} from "@mui/material";
 import Footer from "../Footer/Footer";
 import uploadLogo from "../../../images/cloud-icon1.ecf0bf2.svg";
+import {useState} from "react";
+import {initializeApp} from "firebase/app";
+import {getStorage, ref, uploadBytes, getDownloadURL} from "firebase/storage";
+import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
 
 
 export default function UploadVideoComponent() {
+    const [videoCaption, setVideoCaption] = useState('');
+    const loggedUserData = useSelector(state => state.userData);
+    const [urlOfNewUpload, setUrlOfNewUpload] = useState('');
+    const [videoRef, setVideoRef] = useState('');
+    const [showSuccessAlert, setShowAlert] = useState(false);
+    const [redirectToHome, setRedirect] = useState(false);
+
+    let navigate = useNavigate();
+
+    const changeVideoCaption = (ev) => {
+        setVideoCaption(ev.target.value);
+    }
+    const firebaseConfig = {
+        apiKey: "AIzaSyBH6ffbjtiyt2Gzlin65-uLTmEmPoWA7-Y",
+        authDomain: "tiktok-635d3.firebaseapp.com",
+        databaseURL: "https://tiktok-635d3-default-rtdb.firebaseio.com",
+        projectId: "tiktok-635d3",
+        storageBucket: "tiktok-635d3.appspot.com",
+        messagingSenderId: "152042874053",
+        appId: "1:152042874053:web:435f2f6867ef6ee4e1bcac"
+    };
+
+    const firebaseApp = initializeApp(firebaseConfig);
+    const storage = getStorage(firebaseApp);
+
+    const updateUsersAndVideosDB = () => {
+        let newVidObj = {
+            comments: [{
+                comment: 'testingComment',
+                profilePicture: 'https://firebasestorage.googleapis.com/v0/b/tiktok-635d3.appspot.com/o/empty_profile_picture.jpeg?alt=media&token=0240b2a9-4dad-44a0-bbda-f5aa14892a0d',
+                username: '-MwYW5QhTzoZePKj0FpS'
+            }],
+            commentsCounter: 0,
+            description: videoCaption,
+            likedBy: ['-MwYW5QhTzoZePKj0FpS'],
+            likesCounter: 0,
+            nickname: loggedUserData.nickname,
+            owner: loggedUserData.username,
+            profilePicture: loggedUserData.profilePicture,
+            sharesCounter: 0,
+            song: 'original sound',
+            video: urlOfNewUpload
+        }
+
+        fetch('https://tiktok-635d3-default-rtdb.firebaseio.com/videos.json', {
+            method: 'POST',
+            body: JSON.stringify(newVidObj)
+        })
+            .then(res => res.json())
+            .then(data => {
+                let uploadedVideoId = data.name;
+                let currentVideos = loggedUserData.myVideos;
+                currentVideos.push(uploadedVideoId);
+
+
+                fetch(`https://tiktok-635d3-default-rtdb.firebaseio.com/users/${loggedUserData.username}/myVideos/.json`, {
+                    method: 'PUT',
+                    body: JSON.stringify(currentVideos)
+                })
+                    .then(res => res.json())
+                    .then(data => data);
+            })
+
+    }
+
+    const handleFileInput = (ev) => {
+
+        const fileId = Math.floor(Math.random() * 100);
+        const videoRef = ref(storage, `test-video-${fileId}.mp4`);
+        const file = ev.target.files[0];
+
+        uploadBytes(videoRef, file)
+            .then(res => {
+                getDownloadURL(videoRef)
+                    .then((url) => {
+                        setUrlOfNewUpload(url);
+                        document.getElementById('uploadPostBtn').disabled = false;
+                        setVideoRef(file.name);
+                    })
+            })
+    }
+
+    const postVideo = () => {
+        //todo -> uncomment here ->
+        // updateUsersAndVideosDB();
+        setShowAlert(true);
+        //todo -> setTimeout and redirect to home
+    }
+
+    // if(redirectToHome){
     return (
         <>
             <div className={styles.uploadVideoWrapper}>
@@ -15,64 +110,91 @@ export default function UploadVideoComponent() {
                     </div>
                     <div className={styles.uploadBody}>
                         <div className={styles.uploadSideContainer}>
-                            <img src={uploadLogo} alt={'upload'} style={{marginBottom: 18}}/>
-                            <div>Select video to upload</div>
-                            <div className={styles.uploadDragText} style={{marginBottom: 14}}>Or drag and drop a file
-                            </div>
-                            <div className={styles.uploadSmallText}>
-                                <div style={{marginBottom: 8}}>MP4 or WebM</div>
-                                <div style={{marginBottom: 8}}>720x1280 resolution or higher</div>
-                                <div style={{marginBottom: 8}}>Up to 180 seconds</div>
-                                <div style={{marginBottom: 8}}>Less than 1 GB</div>
-                            </div>
-                            <button className={styles.uploadSelectBtn}>Select file</button>
+                            {urlOfNewUpload==='' ? (
+                                <div className={styles.uploadSideTopics}>
+                                    <img src={uploadLogo} alt={'upload'} style={{marginBottom: 18}}/>
+                                    <div>Select video to upload</div>
+                                    <div className={styles.uploadDragText} style={{marginBottom: 14}}>Or drag and drop a file
+                                    </div>
+                                    <div className={styles.uploadSmallText}>
+                                        <div style={{marginBottom: 8}}>MP4 or WebM</div>
+                                        <div style={{marginBottom: 8}}>720x1280 resolution or higher</div>
+                                        <div style={{marginBottom: 8}}>Up to 180 seconds</div>
+                                        <div style={{marginBottom: 8}}>Less than 1 GB</div>
+                                    </div>
+                                </div>
+                            ):(
+                                <div className={styles.uploadSideTopics}>
+                                    <img src={uploadLogo} alt={'upload'} style={{marginBottom: 18}}/>
+                                    <div className={styles.uploadSelectedVidText} style={{fontWeight:600}}>Selected video:</div>
+                                    <div className={styles.uploadSelectedVidText}>{videoRef}</div>
+                                </div>
+                            )}
+                            <label className={styles.uploadSelectBtn}>
+                                <input type={'file'} className={styles.uploadHiddenInput}
+                                       accept="video/mp4" onChange={ev => handleFileInput(ev)}/>
+                                Select file </label>
                         </div>
                         <div className={styles.uploadMainContainer}>
                             <div className={styles.uploadCaption}>
                                 <div>Caption</div>
-                                <div style={{fontSize: 13}}>0/150</div>
+                                <div style={{fontSize: 13, color: '#5c5d66'}}>{videoCaption.length}/150</div>
                             </div>
-                            <input className={styles.uploadInputCaption}/>
-                            <div className={styles.uploadCaption}>Cover</div>
-                            <div className={styles.uploadCoverBox}/>
-                            <FormControl sx={{minWidth: 120}}>
-                                <Select labelId="demo-simple-select-label" id="visibility-select">
-                                    <MenuItem value={'Public'} defaultChecked={'checked'}>Public</MenuItem>
-                                    <MenuItem value={'Friends'}>Friends</MenuItem>
-                                    <MenuItem value={'Private'}>Private</MenuItem>
-                                </Select>
-                            </FormControl>
-                            <div className={styles.uploadCheckBoxWrapper}>
-                                <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ff2c55"}}/>}
-                                                  label="Comment"/>
-                                <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ff2c55"}}/>}
-                                                  label="Duet"/>
-                                <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ff2c55"}}/>}
-                                                  label="Stitch"/>
+                            <input className={styles.uploadInputCaption} onChange={ev => changeVideoCaption(ev)}/>
+                            <div className={styles.uploadOptionsWrapper}>
+                                <FormControl sx={{minWidth: 120}}>
+                                    <Select labelId="demo-simple-select-label" id="visibility-select"
+                                            defaultValue={'Public'}>
+                                        <MenuItem value={'Public'}>Public</MenuItem>
+                                        <MenuItem value={'Friends'}>Friends</MenuItem>
+                                        <MenuItem value={'Private'}>Private</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                <div className={styles.uploadCheckBoxWrapper}>
+                                    <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ff2c55"}}/>}
+                                                      label="Comment"/>
+                                    <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ff2c55"}}/>}
+                                                      label="Duet"/>
+                                    <FormControlLabel control={<Checkbox defaultChecked style={{color: "#ff2c55"}}/>}
+                                                      label="Stitch"/>
+                                </div>
                             </div>
+
                             <div className={styles.uploadRunCheck}>
                                 Run a copyright check
                                 <Switch color="default"/>
                             </div>
                             <div className={styles.uploadCheckerInfo}>We'll check your video for potential copyright
                                 infringements on used sounds. If infringements are found, you can edit the video before
-                                posting. Learn more
+                                posting.
                             </div>
                             <div className={styles.uploadButtons}>
                                 <button className={styles.uploadDiscardBtn}>Discard</button>
-                                <button className={styles.uploadPostBtn} disabled={'disabled'}>Post</button>
+                                {urlOfNewUpload === '' ?
+                                    (<button id={'uploadPostBtn'} className={styles.uploadPostBtnDisabled}
+                                             disabled={'disabled'}>Post</button>)
+                                    : (<button id={'uploadPostBtn'} className={styles.uploadPostBtnEnabled}
+                                               onClick={postVideo}>Post</button>)
+                                }
+
                             </div>
+                            {showSuccessAlert ?
+                                (<Alert className={styles.uploadSuccessAlert} variant="outlined">Video uploaded successfully!</Alert>)
+                                : (<></>)
+                            }
 
                         </div>
                     </div>
                 </div>
             </div>
             <Footer/>
-            {/*<SignUp/>*/}
-            {/*<Register/>*/}
-            {/*<Login/>*/}
-            {/*<LoginWithEmail/>*/}
         </>
     )
+
+    // }else {
+    //     // setTimeout
+    //     return navigate('/');
+    // }
+
 }
 
